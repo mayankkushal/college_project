d49999c40857400c232853af776870df9fbf828c
mayankkushal <mayankkushal26@gmail.com>
2017-04-22 00:26:05 +0530

    bug fixes

--- se_project/settings.py
+++ se_project/settings.py
@@ -140,3 +140,17 @@ MEDIA_ROOT = MEDIA_DIR
 
 admin.site.site_header = 'BMSIT administration' 
 admin.site.site_title = 'BMSIT'
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
+DEBUG = False
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
